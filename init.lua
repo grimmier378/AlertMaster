da35bd6b7835d8b2c09b44ea2b32e307e7435975
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-02-28 01:09:55 -0700

    version update

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ Icons = require('mq.ICONS')
 local COLOR = require('color.colors')
 -- Variables
 local arg = {...}
-local amVer = '1.8.2'
+local amVer = '1.84'
 local CMD = mq.cmd
 local TLO = mq.TLO
 local Me = TLO.Me
